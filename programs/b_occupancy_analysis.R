162a5a5adfe2cba0eb2069a4a4eb19ad4ab00fa8
aaarchmiller <althea.archmiller@gmail.com>
2020-09-19 10:49:12 -0500

    documented model for readability

--- programs/b_occupancy_analysis.R
+++ programs/b_occupancy_analysis.R
@@ -78,8 +78,8 @@ ks.bw[,,lookup.hhttii.array[1,1,1]]
 #' ## Write model
 #' 
 model.occ <- function(){
-  # Priors
-  beta.prebroad ~ dt(0, pow(2.5, -2), 1)
+  # Priors for determining the probability of detection based on broadcast species
+  beta.prebroad ~ dt(0, pow(2.5, -2), 1) #p(detection) before any broadcast
   beta.pacific ~  dt(0, pow(2.5, -2), 1)
   beta.mottled ~ dt(0, pow(2.5, -2), 1)
   beta.crested ~ dt(0, pow(2.5, -2), 1)
@@ -94,6 +94,7 @@ model.occ <- function(){
   
   for(hh in 1:n.route){
     for(tt in 1:n.year){
+      #Hyperpriors for Psi ensure a flexible and uninformed prior
       mu.psi[hh,tt] ~ dunif(0.01,0.99)
       lnrho.psi[hh,tt] ~ dnorm(5,1)%_%T(0.01,10)
       rho.psi[hh,tt] <- exp(lnrho.psi[hh,tt])
@@ -101,18 +102,21 @@ model.occ <- function(){
       b.psi[hh,tt] <- rho.psi[hh,tt]-(mu.psi[hh,tt]*rho.psi[hh,tt])
       
       
-      
+      # Prior for Psi, which will vary by route (hh) and year (tt)
       psi[hh,tt] ~ dbeta(a.psi[hh,tt], b.psi[hh,tt])%_%T(0.0001,0.99)
       
+      
       for(ii in 1:n.survey){ # 1 to 3 surveys per year
-        
-        
         for(jj in 1:10){ # 10 stations per route
           for(kk in 1:n.broadcast){ # before or after broadcast
             
+            # Function that creates logistic equation for p(detection)
             
+            # p = p(detection), which varies by route, year, survey, station, and 
+            #       broadcast period (pre- or post-broadcast)
             p[hh,tt,ii,jj,kk] <- exp(logit.p[hh,tt,ii,jj,kk])/(1+exp(logit.p[hh,tt,ii,jj,kk]))
             
+            # Logistic regression equation
             logit.p[hh,tt,ii,jj,kk] <- 
               beta.prebroad*ks.prebroad[jj,kk,lookup.hhttii.array[hh,tt,ii]]+
               beta.pacific*ks.pacific[jj,kk,lookup.hhttii.array[hh,tt,ii]]+
@@ -136,37 +140,28 @@ model.occ <- function(){
   # Likelihood
   for(hh in 1:n.route){ # 6 routes
     for(tt in 1:n.year){ # all years
-      
-      
-      
-      
-      # Detection by route, year, survey station, and broadcast
       for(ii in 1:n.survey){ # 1 to 3 surveys per year
-        
-        
-        
         for(jj in 1:n.station){ # 10 stations per route
-          # Occupancy by route and year and survey
+          # Occupancy by route and year and survey and station based on 
+          #     psi, probability of occupancy for each route/year
           z[hh,tt,ii,jj] ~ dbern(psi[hh,tt]) 
           
           for(kk in 1:n.broadcast){ # before or after broadcast
-            # observations by route, year, survey, station, pre/post broadcast
+            # Detection by route, year, survey station, and broadcast period
+            
+            # Binary observations by route, year, survey, station, pre/post broadcast
             y[jj,kk,lookup.hhttii.array[hh,tt,ii]] ~ 
               dbern(eff.p[hh,tt,ii,jj,kk])
             
+            # Effective p(detection), which depends on occupany (z) = 1 for 
+            #    that route/year/survey/station
             eff.p[hh,tt,ii,jj,kk] <- p[hh,tt,ii,jj,kk]*z[hh,tt,ii,jj]
             
           }
         }
-        
-        
-        
       }
-      
     }
-    
   }
-  
 }
 
 
