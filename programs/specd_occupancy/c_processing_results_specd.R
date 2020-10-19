f9f90472cd8978b31ff3b7e07adc574cb8b3417a
aaarchmiller <althea.archmiller@gmail.com>
2020-10-19 14:56:42 -0500

    processed occupancy results for Specd

--- programs/specd_occupancy/c_processing_results_specd.R
+++ programs/specd_occupancy/c_processing_results_specd.R
@@ -31,8 +31,8 @@ set.seed(258854)
 #' ## Load Data
 #' 
 #' 
-#' Jagsout FerPy
-load(file = "data/output_data/ferpy_jagsout.Rdata")
+#' Jagsout Specd
+load(file = "data/output_data/specd_jagsout.Rdata")
 
 #' Survey list
 (route.names <- c("EI1", "EI2", "M2",  "N1",  "N2") )
@@ -45,11 +45,11 @@ year.index <- 1:length(year.names)
 #' 
 #' Create traceplots of parameters (creates PDF)
 #'
-#' Ferpy
-# MCMCtrace(ferpy.jagsout,
+#' Specd
+# MCMCtrace(specd.jagsout,
 #           pdf = TRUE,
 #           open_pdf = FALSE,
-#           filename = "ferpyTrace",
+#           filename = "specdTrace",
 #           wd = "output")
 
 
@@ -67,31 +67,31 @@ all.rows <- 1:(length(route.names)*length(year.index))
 (include.byrow <- all.rows[!all.rows %in% exclude.byrow])
 #' 
 
-#' Ferpy
+#' Specd
 #' 
-psi.post.ferpy <- MCMCsummary(ferpy.jagsout, 
+psi.post.specd <- MCMCsummary(specd.jagsout, 
                               params = "psi", 
                               Rhat = TRUE,
                               n.eff = TRUE,
                               probs = c(0.05, 0.5, 0.95))
-psi.post.ferpy$Year <- rep(1:length(year.index), each = length(route.index))
-psi.post.ferpy$Route <- rep(c("EI.1", "EI.2", "M.2", "N.1", "N.2"), length(year.index))
-psi.post.ferpy$Species <- "FerPy"
-psi.post.ferpy <- psi.post.ferpy[include.byrow,]
+psi.post.specd$Year <- rep(1:length(year.index), each = length(route.index))
+psi.post.specd$Route <- rep(c("EI.1", "EI.2", "M.2", "N.1", "N.2"), length(year.index))
+psi.post.specd$Species <- "Specd"
+psi.post.specd <- psi.post.specd[include.byrow,]
 
 
 
 
 #' Populate Region
 #' 
-psi.post.ferpy$Region[psi.post.ferpy$Route == "EI.1" |
-                        psi.post.ferpy$Route == "EI.2"] <- "El Imposible"
-psi.post.ferpy$Region[psi.post.ferpy$Route == "N.1" |
-                        psi.post.ferpy$Route == "N.2"] <- "Nancuchiname"
-psi.post.ferpy$Region[psi.post.ferpy$Route == "M.1" |
-                        psi.post.ferpy$Route == "M.2"] <- "Montecristo"
-colnames(psi.post.ferpy)
-colnames(psi.post.ferpy) <- c("Psi.mean", "Psi.sd", "Psi.LL05", "Psi.median",
+psi.post.specd$Region[psi.post.specd$Route == "EI.1" |
+                        psi.post.specd$Route == "EI.2"] <- "El Imposible"
+psi.post.specd$Region[psi.post.specd$Route == "N.1" |
+                        psi.post.specd$Route == "N.2"] <- "Nancuchiname"
+psi.post.specd$Region[psi.post.specd$Route == "M.1" |
+                        psi.post.specd$Route == "M.2"] <- "Montecristo"
+colnames(psi.post.specd)
+colnames(psi.post.specd) <- c("Psi.mean", "Psi.sd", "Psi.LL05", "Psi.median",
                               "Psi.UL95", "Psi.Rhat", "Psi.neff", "Year", "Route",
                               "Species", "Region")
 
@@ -118,9 +118,9 @@ inc.N.2 <- all.years[!all.years %in% exc.N.2]
 #' 
 
 
-#' Psi posteriors for Ferpy
+#' Psi posteriors for Specd
 #' 
-ferpy.chains <- MCMCpstr(ferpy.jagsout, 
+specd.chains <- MCMCpstr(specd.jagsout, 
                          params = "psi",
                          type = "chains")
 
@@ -128,34 +128,34 @@ ferpy.chains <- MCMCpstr(ferpy.jagsout,
 
 #' Calculate by species and route
 #' 
-psi.means.ferpy <- as.data.frame(matrix(NA, ncol = 5, nrow = length(route.names)))
-colnames(psi.means.ferpy) <- c("Species", "Route", "Psi.LL05", "Psi.median", "Psi.UL95")
-psi.means.ferpy$Species <- "FerPy"
-psi.means.ferpy$Route <- route.names
+psi.means.specd <- as.data.frame(matrix(NA, ncol = 5, nrow = length(route.names)))
+colnames(psi.means.specd) <- c("Species", "Route", "Psi.LL05", "Psi.median", "Psi.UL95")
+psi.means.specd$Species <- "Specd"
+psi.means.specd$Route <- route.names
 
 #' Populate quantile results
 #' 
-psi.means.ferpy[psi.means.ferpy$Route == "EI1",3:5] <- 
-  quantile(ferpy.chains$psi[1,inc.EI.1,], probs = c(0.05, 0.5, 0.95))
-psi.means.ferpy[psi.means.ferpy$Route == "EI2",3:5] <- 
-  quantile(ferpy.chains$psi[2,inc.EI.2,], probs = c(0.05, 0.5, 0.95))
-psi.means.ferpy[psi.means.ferpy$Route == "M2",3:5] <- 
-  quantile(ferpy.chains$psi[3,inc.M.2,], probs = c(0.05, 0.5, 0.95))
-psi.means.ferpy[psi.means.ferpy$Route == "N1",3:5] <- 
-  quantile(ferpy.chains$psi[4,inc.N.1,], probs = c(0.05, 0.5, 0.95))
-psi.means.ferpy[psi.means.ferpy$Route == "N2",3:5] <- 
-  quantile(ferpy.chains$psi[5,inc.N.2,], probs = c(0.05, 0.5, 0.95))
+psi.means.specd[psi.means.specd$Route == "EI1",3:5] <- 
+  quantile(specd.chains$psi[1,inc.EI.1,], probs = c(0.05, 0.5, 0.95))
+psi.means.specd[psi.means.specd$Route == "EI2",3:5] <- 
+  quantile(specd.chains$psi[2,inc.EI.2,], probs = c(0.05, 0.5, 0.95))
+psi.means.specd[psi.means.specd$Route == "M2",3:5] <- 
+  quantile(specd.chains$psi[3,inc.M.2,], probs = c(0.05, 0.5, 0.95))
+psi.means.specd[psi.means.specd$Route == "N1",3:5] <- 
+  quantile(specd.chains$psi[4,inc.N.1,], probs = c(0.05, 0.5, 0.95))
+psi.means.specd[psi.means.specd$Route == "N2",3:5] <- 
+  quantile(specd.chains$psi[5,inc.N.2,], probs = c(0.05, 0.5, 0.95))
 
 
 
 #' Populate Region
 #' 
-psi.means.ferpy$Region[psi.means.ferpy$Route == "EI1" |
-                   psi.means.ferpy$Route == "EI2"] <- "El Imposible"
-psi.means.ferpy$Region[psi.means.ferpy$Route == "N1" |
-                   psi.means.ferpy$Route == "N2"] <- "Nancuchiname"
-psi.means.ferpy$Region[psi.means.ferpy$Route == "M1" |
-                   psi.means.ferpy$Route == "M2"] <- "Montecristo"
+psi.means.specd$Region[psi.means.specd$Route == "EI1" |
+                   psi.means.specd$Route == "EI2"] <- "El Imposible"
+psi.means.specd$Region[psi.means.specd$Route == "N1" |
+                   psi.means.specd$Route == "N2"] <- "Nancuchiname"
+psi.means.specd$Region[psi.means.specd$Route == "M1" |
+                   psi.means.specd$Route == "M2"] <- "Montecristo"
 
 
 #' _____________________________________________________________________________
@@ -164,36 +164,36 @@ psi.means.ferpy$Region[psi.means.ferpy$Route == "M1" |
 #' Probability of detection was a function of broadcast species
 #' 
 #' 
-p.det.post.ferpy <- MCMCsummary(ferpy.jagsout, 
+p.det.post.specd <- MCMCsummary(specd.jagsout, 
                                 params = grep("^beta", 
-                                              ferpy.jagsout$parameters.to.save, 
+                                              specd.jagsout$parameters.to.save, 
                                               value = T), 
                                 Rhat = TRUE,
                                 n.eff = TRUE,
                                 probs = c(0.05, 0.5, 0.95))
-p.det.post.ferpy$Species <- "FerPy"
-p.det.post.ferpy$broadcast.param <- grep("^beta", 
-                                         ferpy.jagsout$parameters.to.save, 
+p.det.post.specd$Species <- "Specd"
+p.det.post.specd$broadcast.param <- grep("^beta", 
+                                         specd.jagsout$parameters.to.save, 
                                          value = T)
-p.det.post.ferpy
+p.det.post.specd
 
 
 
 #' Merge data together and change column names
 #' 
-colnames(p.det.post.ferpy)
-colnames(p.det.post.ferpy) <- c("mean", "sd", "LL05", "median", "UL95", "Rhat",
+colnames(p.det.post.specd)
+colnames(p.det.post.specd) <- c("mean", "sd", "LL05", "median", "UL95", "Rhat",
                           "n.eff", "Species", "broadcast.param")
 
 #' Transform to probability scale
 #' 
-p.det.post.ferpy$LL05.plogis <- plogis(p.det.post.ferpy$LL05)
-p.det.post.ferpy$median.plogis <- plogis(p.det.post.ferpy$median)
-p.det.post.ferpy$UL95.plogis <- plogis(p.det.post.ferpy$UL95)
+p.det.post.specd$LL05.plogis <- plogis(p.det.post.specd$LL05)
+p.det.post.specd$median.plogis <- plogis(p.det.post.specd$median)
+p.det.post.specd$UL95.plogis <- plogis(p.det.post.specd$UL95)
 
 #' Save broadcast species as a factor, ordered by confidence
 #' 
-p.det.post.ferpy$Broadcast <- factor(p.det.post.ferpy$broadcast.param,
+p.det.post.specd$Broadcast <- factor(p.det.post.specd$broadcast.param,
                                   levels = c("beta.prebroad", "beta.mottled",
                                              "beta.pacific", "beta.crested",
                                              "beta.bw", "beta.spectacled",
@@ -208,14 +208,14 @@ p.det.post.ferpy$Broadcast <- factor(p.det.post.ferpy$broadcast.param,
 #' ## Save files
 #' 
 #' Psi Posteriors by year and route
-save(psi.post.ferpy, file = "data/output_data/ferpy_psi_posteriors_RtYr.Rdata")
+save(psi.post.specd, file = "data/output_data/specd_psi_posteriors_RtYr.Rdata")
 
 #' Psi posteriors across years by species and route
-save(psi.means.ferpy, file = "data/output_data/ferpy_psi_posteriors_RtSpp.Rdata")
+save(psi.means.specd, file = "data/output_data/specd_psi_posteriors_RtSpp.Rdata")
 
 #' Probability of detection by broadcast species and species of analysis
 #' 
-save(p.det.post.ferpy, file = "data/output_data/ferpy_p_detection_posteriors.Rdata")
+save(p.det.post.specd, file = "data/output_data/specd_p_detection_posteriors.Rdata")
 
 
 #' _____________________________________________________________________________
@@ -225,5 +225,5 @@ save(p.det.post.ferpy, file = "data/output_data/ferpy_p_detection_posteriors.Rda
 devtools::session_info()
 #' This document was "spun" with:
 #' 
-#' ezspin(file = "programs/ferpy_occupancy/c_processing_results_ferpy.R", out_dir = "output", fig_dir = "figures", keep_md = F)
+#' ezspin(file = "programs/specd_occupancy/c_processing_results_specd.R", out_dir = "output", fig_dir = "figures", keep_md = F)
 #' 
